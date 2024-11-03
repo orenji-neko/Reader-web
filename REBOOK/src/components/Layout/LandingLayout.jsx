55fe623c98bfc98e34b7b88c7e6413d883d84b96
IanDal <ianjohndal5@gmail.com>
2024-11-03 22:46:29 +0800

    Update LandingLayout.jsx
    Made it responsive and Added Animation

--- REBOOK/src/components/Layout/LandingLayout.jsx
+++ REBOOK/src/components/Layout/LandingLayout.jsx
@@ -5,26 +5,20 @@ import Sidebar from "../Sidebar";
 
 const LandingLayout = () => {
   const [isSidebarOpen, setSidebarOpen] = useState(true);
-  const [isDropdownOpen, setDropdownOpen] = useState(false);
-  const [searchTerm, setSearchTerm] = useState('');
 
-  // Toggle the sidebar
   const toggleSidebar = () => {
     setSidebarOpen(!isSidebarOpen);
   };
 
   return (
     <div className="flex flex-col sm:flex-row bg-teal-100 overflow-x-hidden">
-      {isSidebarOpen && (
-        <div className={`w-64 ${isSidebarOpen ? 'block' : 'hidden'} sm:block`}>
+      <div className={`fixed inset-y-0 left-0 transition-transform duration-300 ${isSidebarOpen ? 'translate-x-0' : '-translate-x-full'} w-64`}>
         <Sidebar />
       </div>
-      )}
-      <div className="flex-1 flex flex-col">
+      <div className={`flex-1 flex flex-col transition-all duration-300 ${isSidebarOpen ? 'ml-64' : 'ml-0'}`}>
         <Navbar toggleSidebar={toggleSidebar} isSidebarOpen={isSidebarOpen} />
-        
         <main className="p-4 flex-1 overflow-hidden">
-          <Outlet />
+          <Outlet context={{ isSidebarOpen }} />
         </main>
       </div>
     </div>
