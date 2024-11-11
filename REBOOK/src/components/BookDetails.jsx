90525dd8d47dbdb13bdbc848c046138e89406b3d
Mark Enfermo <markanthony.enfermo@gmail.com>
2024-11-11 10:08:38 +0800

    fix: book details

--- REBOOK/src/components/BookDetails.jsx
+++ REBOOK/src/components/BookDetails.jsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 
 function BookDetails() {
