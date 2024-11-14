2e10c4ecd30c6e89cd52cbb8ed416824ade03cad
Ian Dal <ianjohndal5@gmail.com>
2024-11-14 14:06:00 +0800

    Finished Login & Register

--- REBOOK/src/components/Landing/Login.jsx
+++ REBOOK/src/components/Landing/Login.jsx
@@ -1,16 +1,21 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { FaTimes } from 'react-icons/fa'; 
 import './LoginRegister.css'; // Import the CSS file
 
-function Login({ onClose }) {
-  const [username, setUsername] = useState('');
+function Login() {
+  const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const navigate = useNavigate();
 
   const handleLogin = (event) => {
     event.preventDefault();
     // Implement login logic here
-    console.log('Login attempt', { username, password });
+    console.log('Login attempt', { email, password });
+  };
+
+  const handleClose = () => {
+    navigate('/');
   };
 
   return (
@@ -20,31 +25,30 @@ function Login({ onClose }) {
           <img src="/rebook-images/Component2.png" alt="ReBook Logo" className="h-16" />
         </a>
       </div>
-      <div className="relative flex flex-col bg-white p-5 rounded-3xl shadow-md space-y-3 w-full max-w-sm">
-        <button onClick={onClose} className="absolute top-3 right-3 text-black-500 hover:text-black-700">
+      <div className="relative flex flex-col bg-white p-5 rounded-3xl shadow-md space-y-3 w-full max-w-xs" style={{ height: '320px' }}>
+        <button onClick={handleClose} className="absolute top-3 right-3 text-black-500 hover:text-black-700">
           <FaTimes />
         </button>
-        <div className="flex justify-center mb-3">
-          <h2 className="text-3xl">LOGIN</h2>
-        </div>
         <div className="login-form">
           <form onSubmit={handleLogin}>
+            <header>Email</header>
             <div className="mb-3">
               <input
                 type="text"
-                value={username}
-                placeholder="Username"
-                className="w-full p-2 rounded-lg border border-gray-300 text-center"
+                value={email}
+                placeholder="user@gmail.com..."
+                className="w-full p-2 rounded-lg border border-gray-300 "
                 onChange={(e) => setUsername(e.target.value)}
                 required
               />
             </div>
+            <header>Password</header>
             <div className="mb-3">
               <input
                 type="password"
                 value={password}
-                placeholder="Password"
-                className="w-full p-2 rounded-lg border border-gray-300 text-center"
+                placeholder="password..."
+                className="w-full p-2 rounded-lg border border-gray-300 "
                 onChange={(e) => setPassword(e.target.value)}
                 required
               />
@@ -52,6 +56,7 @@ function Login({ onClose }) {
             <button type="submit" className="w-full bg-teal-500 text-white py-2 rounded-lg hover:bg-blue-700">
               Login
             </button>
+            <Link to="/forgot" className="block text-center text-black-500 hover:underline mt-2">Forgot Password?</Link>
             <Link to="/register" className="block text-center text-black-500 hover:underline mt-2">Create Account</Link>
           </form>
         </div>
