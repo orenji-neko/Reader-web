8c747ae6aca480cbc0fdbe8549cd315916d8050d
Mark Enfermo <112786278+orenji-neko@users.noreply.github.com>
2024-11-17 18:37:59 +0800

    feature: added add book in inventory

--- api/src/api/v1/category.ts
+++ api/src/api/v1/category.ts
@@ -1,17 +1,28 @@
-import Elysia from "elysia";
+import Elysia, { t } from "elysia";
 import { PrismaClient } from "@prisma/client";
+import auth from "../../utils/auth";
 
 const app = new Elysia()
     .decorate("prisma", new PrismaClient())
+    .use(auth)
     /**
      * [GET]    /api/v1/categories
      * [DESC]   Get a list of categories.
      */
-    .get("/categories", async ({ prisma }) => {
-        
+    .get("/categories", async ({ headers, prisma, jwt }) => {
+        const user:any = await jwt.verify(headers.authorization)
+
+        if(!user) {
+            throw new Error("Unauthorized!")
+        }
+
         const categories = await prisma.category.findMany();
 
         return categories;
+    }, {
+        headers: t.Object({
+            authorization: t.String()
+        })
     })
     .get("/category/:id", async ({ params, prisma }) => {
         const id = params.id;
